2df5d81cfc55fb61606a43336b5088ca10289a79
Wictor Wilén <wictorwilen@users.noreply.github.com>
2009-08-28 15:02:37 +0000

    Initial checkin

---
+++ TweetPart/TweetPart/TweetPart/TwitterEditorPart.cs
@@ -0,0 +1,129 @@
+﻿/*
+ * 
+ * TweetPart for SharePoint
+ * ------------------------------------------
+ * Copyright (c) 2009, Wictor Wilén
+ * http://tweetpart.codeplex.com/
+ * http://www.wictorwilen.se/
+ * ------------------------------------------
+ * Licensed under the Microsoft Public License (Ms-PL) 
+ * http://www.opensource.org/licenses/ms-pl.html
+ * 
+ */
+using System;
+
+using System.Web.UI.WebControls;
+using System.Web.UI;
+using System.Globalization;
+using System.Web.UI.WebControls.WebParts;
+
+namespace TweetPart {
+    public class TwitterEditorPart : EditorPart {
+
+        protected ListBox m_lbModes;
+        protected TextBox m_tbUsername;
+        protected TextBox m_tbSearchString;
+        protected Panel m_editorPanel;
+        protected Panel m_modesPanel;
+        protected Panel m_usernamePanel;
+        protected Panel m_searchPanel;
+
+
+        protected override void CreateChildControls() {
+
+            this.Title = "TweetPart properties";
+
+            // Add controls here
+            m_editorPanel = new Panel();
+            m_editorPanel.CssClass = "ms-ToolPartSpacing";
+            this.Controls.Add(m_editorPanel);
+
+            m_modesPanel = new Panel();
+            m_modesPanel.Controls.Add(new LiteralControl("<div class='UserSectionHead'>Mode</div>"));
+            m_modesPanel.Controls.Add(new LiteralControl("<div class='UserSectionBody'><div class='UserControlGroup'><nobr>"));
+            m_lbModes = new ListBox();
+            m_lbModes.CssClass = "UserInput";
+            m_lbModes.Width = new Unit("176px", CultureInfo.InvariantCulture);
+            Array.ForEach(Enum.GetNames(typeof(TwitterTimeLineMode)), m_lbModes.Items.Add);
+
+            m_lbModes.AutoPostBack = true;
+            m_lbModes.SelectedIndexChanged += new EventHandler(m_lbModes_SelectedIndexChanged);
+            m_modesPanel.Controls.Add(m_lbModes);
+            m_modesPanel.Controls.Add(new LiteralControl("</nobr></div></div>"));
+            m_editorPanel.Controls.Add(m_modesPanel);
+
+            m_searchPanel = new Panel();
+            m_searchPanel.Controls.Add(new LiteralControl("<div class='UserSectionHead'>Search string</div>"));
+            m_searchPanel.Controls.Add(new LiteralControl("<div class='UserSectionBody'><div class='UserControlGroup'><nobr>"));
+            m_tbSearchString = new TextBox();
+            m_tbSearchString.CssClass = "UserInput";
+            m_tbSearchString.Width = new Unit("176px", CultureInfo.InvariantCulture);
+            m_searchPanel.Controls.Add(m_tbSearchString);
+            m_searchPanel.Controls.Add(new LiteralControl("</nobr></div></div>"));
+            m_editorPanel.Controls.Add(m_searchPanel);
+
+
+            m_usernamePanel = new Panel();
+            m_usernamePanel.Controls.Add(new LiteralControl("<div class='UserSectionHead'>Username</div>"));
+            m_usernamePanel.Controls.Add(new LiteralControl("<div class='UserSectionBody'><div class='UserControlGroup'><nobr>"));
+            m_tbUsername = new TextBox();
+            m_tbUsername.CssClass = "UserInput";
+            m_tbUsername.Width = new Unit("176px", CultureInfo.InvariantCulture);
+
+            m_usernamePanel.Controls.Add(m_tbUsername);
+            m_usernamePanel.Controls.Add(new LiteralControl("</nobr></div></div>"));
+            m_editorPanel.Controls.Add(m_usernamePanel);
+
+
+
+            base.CreateChildControls();
+            this.ChildControlsCreated = true;
+        }
+
+        void m_lbModes_SelectedIndexChanged(object sender, EventArgs e) {
+            showHideInputs();
+        }
+
+        private void showHideInputs() {
+            switch ((TwitterTimeLineMode)Enum.Parse(typeof(TwitterTimeLineMode), m_lbModes.SelectedValue)) {
+                case TwitterTimeLineMode.Search:
+                    m_searchPanel.Visible = true;
+                    m_usernamePanel.Visible = false;
+                    break;
+                case TwitterTimeLineMode.User:
+                    m_searchPanel.Visible = false;
+                    m_usernamePanel.Visible = true;
+                    break;
+                default:
+                    m_searchPanel.Visible = false;
+                    m_usernamePanel.Visible = false;
+                    break;
+            }
+        }
+
+        public override void SyncChanges() {
+            EnsureChildControls();
+            TwitterWebPart twitterWebPart = (TwitterWebPart)this.WebPartToEdit;
+            if (twitterWebPart != null) {
+                // get values from WebPart
+                m_lbModes.SelectedValue = twitterWebPart.Mode.ToString();
+                m_tbUsername.Text = twitterWebPart.Username;
+                m_tbSearchString.Text = twitterWebPart.SearchText;
+            }
+            showHideInputs();
+            return;
+        }
+        public override bool ApplyChanges() {
+            EnsureChildControls();
+            TwitterWebPart twitterWebPart = (TwitterWebPart)this.WebPartToEdit;
+            if (twitterWebPart != null) {
+                twitterWebPart.Mode = (TwitterTimeLineMode)Enum.Parse(typeof(TwitterTimeLineMode), m_lbModes.SelectedValue);
+                twitterWebPart.SearchText = m_tbSearchString.Text;
+                twitterWebPart.Username = m_tbUsername.Text;
+            }
+            return true;
+        }
+    }
+
+
+}
---
+++ TweetPart/TweetPart/TweetPart/WebPartCode/TwitterWebPart.cs
@@ -0,0 +1,107 @@
+/*
+ * 
+ * TweetPart for SharePoint
+ * ------------------------------------------
+ * Copyright (c) 2009, Wictor Wilén
+ * http://tweetpart.codeplex.com/
+ * http://www.wictorwilen.se/
+ * ------------------------------------------
+ * Licensed under the Microsoft Public License (Ms-PL) 
+ * http://www.opensource.org/licenses/ms-pl.html
+ * 
+ */
+using System;
+using System.Web.UI.WebControls.WebParts;
+using System.ComponentModel;
+using Microsoft.SharePoint.WebPartPages;
+using System.Collections.Generic;
+using Microsoft.SharePoint.Security;
+using System.Security.Permissions;
+using System.Net;
+
+namespace TweetPart {
+
+    [SharePointPermission(SecurityAction.Demand, ObjectModel=true)]
+    [WebPermission(SecurityAction.Demand, AcceptPattern = "http://www.twitter.com/*", ConnectPattern = "http://www.twitter.com/*", Unrestricted=true)]
+    [WebPermission(SecurityAction.Demand, AcceptPattern = "http://search.twitter.com/*", ConnectPattern = "http://search.twitter.com/*", Unrestricted=true)]
+    public class TwitterWebPart : System.Web.UI.WebControls.WebParts.WebPart, IWebEditable {
+       
+
+        protected override void CreateChildControls() {
+
+
+            if (string.IsNullOrEmpty(this.Username)) {
+                this.Mode = TwitterTimeLineMode.Public;
+            }
+            TwitterStuff.GenerateTweets(this.Username, this.Controls, this.Mode, this.TweetCount, this.SearchText);
+
+        }
+
+
+
+        [WebBrowsable]
+        [WebDisplayName("User name")]
+        [WebDescription("The Twitter Username")]
+        [Category("Twitter")]
+        //[Personalizable(PersonalizationScope.User)]
+        [WebPartStorage(Storage.Personal)]
+        public string Username {
+            get;
+            set;
+        }
+
+        [WebBrowsable]
+        [WebDisplayName("Search for")]
+        [WebDescription("Text to search for")]
+        [Category("Twitter")]
+        //[Personalizable(PersonalizationScope.User)]
+        [WebPartStorage(Storage.Personal)]
+        public string SearchText {
+            get;
+            set;
+        }
+
+        [WebBrowsable]
+        [WebDisplayName("Time line mode")]
+        [WebDescription("Friends or user time line")]
+        [Category("Twitter")]
+        //[Personalizable(PersonalizationScope.User)]
+        [WebPartStorage(Storage.Personal)]
+        public TwitterTimeLineMode Mode {
+            get;
+            set;
+        }
+
+        [WebBrowsable]
+        [WebDisplayName("Tweet count")]
+        [WebDescription("Number of tweets to show")]
+        [Category("Twitter")]
+        [Personalizable(PersonalizationScope.Shared)]
+        [WebPartStorage(Storage.Shared)]
+        public int TweetCount {
+            get;
+            set;
+        }
+
+
+
+
+        #region IWebEditable Members
+
+        EditorPartCollection IWebEditable.CreateEditorParts() {
+            // control the editorparts
+            List<EditorPart> editors = new List<EditorPart>();
+            TwitterEditorPart editorPart = new TwitterEditorPart();
+            editorPart.ID = this.ID + "_editorPart";
+            editors.Add(editorPart);
+            return new EditorPartCollection(editors);
+
+        }
+
+        object IWebEditable.WebBrowsableObject {
+            get { return this; }
+        }
+
+        #endregion
+    }
+}
